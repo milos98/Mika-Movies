8d62a912abd6151763d3b06f7f6a1889bb08e833
Miloš Janković <mjankovic180@gmail.com>
2019-11-14 00:47:26 +0100

    omdb api

---
+++ jqcustom.js
@@ -0,0 +1,93 @@
+$('document').ready(function () {
+
+  var charData = [];
+  var nowInTheater = ['']
+  var comingSoonIMDB = ['tt4520988', 'tt3224458', 'tt8688634', 'tt9071322', 'tt10703826',
+    'tt8106572', 'tt9358256', 'tt8946378', 'tt8722346',
+    'tt4520988', 'tt3224458', 'tt8688634', 'tt9071322', 'tt10703826',
+    'tt8106572', 'tt9358256', 'tt8946378', 'tt8722346']
+
+  function getInformation(listaIMDBkodova) {
+    listaIMDBkodova.forEach(function (IMDBcode) {
+      fetch(`http://www.omdbapi.com/?apikey=b6e0271a&i=${IMDBcode}`)
+      .then((resp) => resp.json())
+      .then(function(el){
+        createCharacter(el);
+      })
+    });
+  }
+
+  const createCharacter = (el) => {
+    var lik = $('.charElement').prop('outerHTML');
+    console.log(el);
+    let newHTML = lik.replace('template-element', el.imdbID);
+    newHTML = newHTML.replace('%char-modal-id%', el.imdbID);
+    newHTML = newHTML.replace('%char-img-scr%', el.Poster);
+    $('#filmovi').append(newHTML);
+    for (let el of $('#portfolio-item-caption')) {
+      const randBoja = `rgba(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},0.9)`;
+      el.style.backgroundcolor = randBoja;
+    }
+    $('.filmovi').slick({
+      centerMode: true,
+      centerPadding: '60px',
+      slidesToShow: 3,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
+      
+  }
+
+  getInformation(comingSoonIMDB);
+})
+
+/*$.getJSON('https://rickandmortyapi.com/api/character/[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]', function (data) {
+  console.log(data);
+})
+  /*function(data){
+    console.log(data);
+    createCharacter(data);
+})
+$('#filmovi').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});*/
