70206b443441ece3262925310f77daae1166ec3d
Miloš Janković <mjankovic180@gmail.com>
2019-11-18 04:51:40 +0100

    dorada koda za svlacenje sa api-a i modifikacija prikazujeSe.html

--- jqakcija.js
+++ jqakcija.js
@@ -1,21 +1,42 @@
 $('document').ready(function () {
 
-    var charData = [];
-    var nowInTheater = ['']
-    var actionIMDB = ['tt1950186', 'tt6450804', 'tt7286456', 'tt1560220', 'tt7390646']
-  
-    function getInformation(listaIMDBkodova) {
-      listaIMDBkodova.forEach(function (IMDBcode) {
-        fetch(`http://www.omdbapi.com/?apikey=b6e0271a&i=${IMDBcode}`)
-          .then((resp) => resp.json())
-          .then(function (el) {
-            charData.push(el);
-            createCharacter(el);
-            console.log(el);
-          })
-          .then(function (el) {
-            if (charData.length === actionIMDB.length) {
-              $('.row').slick({
+    var IMDBids = ['tt1950186', 'tt6450804', 'tt7286456', 'tt1560220', 'tt7390646']
+
+    async function makeArrayOfData(e) {
+        let charData = [];
+        for (let i = 0; i < e.length; i++) {
+            let IMDBcode = e[i];
+            let response = await fetch(`http://www.omdbapi.com/?apikey=b6e0271a&i=${IMDBcode}`, {});
+            let json = await response.json();
+            charData.push(json);
+        };
+        return charData;
+    }
+
+    function populate(filmovi, klasa) {
+        let film = document.getElementById(klasa).childNodes[3].childNodes[1].childNodes[1];
+        //console.log(film)
+        filmovi.forEach((el) => {
+            let newHTML = film.cloneNode(true);
+            newHTML.id = el.imdbID;
+            newHTML.children[0].src = el.Poster;
+            console.log(newHTML)
+            document.getElementById(klasa).childNodes[3].childNodes[1].appendChild(newHTML);
+            //let Genre = el.Genre.match(newMatch);
+            
+        })
+        console.log(document.getElementById(klasa).childNodes[3].children[0]);
+    }
+
+    async function createCharacter(listaFilmova) {
+        let filmoviJSON = await makeArrayOfData(listaFilmova);
+        let genreList = document.getElementById('zanr').childNodes;
+        for (let i = 1; i < genreList.length; i += 2) {
+            let klasa = genreList[i].id;
+            populate(filmoviJSON, klasa);
+            $('#template-element').remove();
+            console.log("ccc")
+            /*$('#filmovi').slick({
                 centerMode: true,
                 dots: true,
                 centerPadding: '60px',
@@ -25,54 +46,31 @@ $('document').ready(function () {
                 mobileFirst: true,
                 variableWidth: true,
                 responsive: [
-                  {
-                    breakpoint: 768,
-                    settings: {
-                      arrows: true,
-                      centerMode: true,
-                      centerPadding: '40px',
-                      swipeToSlide: true,
-                      variableWidth: true,
-                      slidesToShow: 3
+                    {
+                        breakpoint: 768,
+                        settings: {
+                            arrows: true,
+                            centerMode: true,
+                            centerPadding: '40px',
+                            swipeToSlide: true,
+                            variableWidth: true,
+                            slidesToShow: 3
+                        }
+                    },
+                    {
+                        breakpoint: 480,
+                        settings: {
+                            arrows: false,
+                            centerMode: true,
+                            centerPadding: '40px',
+                            swipeToSlide: true,
+                            variableWidth: true,
+                            slidesToShow: 1
+                        }
                     }
-                  },
-                  {
-                    breakpoint: 480,
-                    settings: {
-                      arrows: false,
-                      centerMode: true,
-                      centerPadding: '40px',
-                      swipeToSlide: true,
-                      variableWidth: true,
-                      slidesToShow: 1
-                    }
-                  }
                 ]
-              })
-            }
-          })
-      });
-    }
-  
-    function createCharacter(el) {
-      var film = $('.charElement').prop('outerHTML');
-      let newHTML = film.replace('template-element', el.imdbID);
-      if (charData.length === actionIMDB.length) {
-        document.getElementById('template-element').children[0].src = el.Poster;
-        document.getElementById('template-element').id = el.imdbID;
-      }
-      else if (charData.length <= (actionIMDB.length - 1)) {
-        newHTML = newHTML.replace('template-element', el.imdbID);
-        newHTML = newHTML.replace('%char-img-scr%', el.Poster);
-        $('#filmovi').append(newHTML);
-      }
-      console.log(charData.length === actionIMDB.length)
-      for (let el of $('#portfolio-item-caption')) {
-        const randBoja = `rgba(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},0.9)`;
-        el.style.backgroundcolor = randBoja;
-      }
-    }
-  
-    getInformation(actionIMDB);
-  })
-  
+            })*/
+        }}
+
+        createCharacter(IMDBids);
+    })
