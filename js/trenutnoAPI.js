2c8647de610290c7955e81d1c616f3e860d64a86
Miloš Janković <mjankovic180@gmail.com>
2019-11-24 20:31:44 +0100

    moved to js folder and renamed

---
+++ js/trenutnoAPI.js
@@ -0,0 +1,47 @@
+$('document').ready(function () {
+
+    var IMDBids = ['tt1950186', 'tt6450804', 'tt7286456', 'tt1560220', 'tt7390646', 'tt4520988', 'tt3224458', 'tt8688634', 'tt9071322', 'tt10703826',
+    'tt8106572', 'tt9358256', 'tt8946378', 'tt8722346']
+
+    async function makeArrayOfData(e) {
+        let charData = [];
+        for (let i = 0; i < e.length; i++) {
+            let IMDBcode = e[i];
+            let response = await fetch(`http://www.omdbapi.com/?apikey=b6e0271a&i=${IMDBcode}`, {});
+            let json = await response.json();
+            charData.push(json);
+            console.log(json.Genre)
+        };
+        return charData;
+    }
+
+    function populate(filmovi, klasa) {
+        let film = document.getElementById(klasa).childNodes[3].childNodes[1].childNodes[1];
+        let newMatch = new RegExp(klasa, "gi")
+        //console.log(film)
+        filmovi.forEach((el) => {
+            let genreMatch = el.Genre.match(newMatch);
+            if (genreMatch) {
+                let newHTML = film.cloneNode(true);
+                newHTML.id = el.imdbID;
+                newHTML.children[0].src = el.Poster;
+                console.log()
+                document.getElementById(klasa).childNodes[3].childNodes[1].appendChild(newHTML);
+            }
+        })
+        console.log();
+    }
+
+    async function createCharacter(listaFilmova) {
+        let filmoviJSON = await makeArrayOfData(listaFilmova);
+        let genreList = document.getElementById('zanr').childNodes;
+        for (let i = 1; i < genreList.length; i += 2) {
+            let klasa = genreList[i].id;
+            populate(filmoviJSON, klasa);
+            $('#template-element').remove();
+            console.log()
+        }
+    }
+
+    createCharacter(IMDBids);
+})
